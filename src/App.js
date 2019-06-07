2ad2546a85cc17460753a22b2ce69768d6b57f65
natan <eliasnatanzigaran@hotmail.com>
2019-06-07 14:29:32 -0300

    Prueba terminada

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { todos } from './todos.json';
+import TodoForm from './components/TodoForm';
 
 
 class App extends Component {
   
   constructor(){
     super();
-    this.state = {
-      todos
-    }
+    this.state = { todos }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
 
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  } 
+
+  removeTodo(index) {
+    this.setState({
+        todos: this.state.todos.filter((e, i) => {
+          return i !== index})
+    })
+  }
 
   render() {
     const todos = this.state.todos.map((todo, i) => {
         return(
         
         
-          <div className="col-md-4">
-
+          <div className="col-md-4" key={i}>
             <div className="card mt-4">
               
               <div className="card-header">
@@ -33,13 +44,18 @@ class App extends Component {
                 <h6 className="">
                 {todo.responsible}
                 </h6>
-
               </div>
               
               <div className="card-body">
                 <p> {todo.description} </p>
               </div>
               
+              <div className="card-footer">
+                <button className="btn btn-danger" onClick={this.removeTodo.bind(this, i)}>
+                  Delete
+                </button>
+              </div>
+
               </div> 
             </div>
            
@@ -59,8 +75,11 @@ class App extends Component {
               
       </nav>
        
+
+
       <div className="container">
            <div className="row mt-5">
+      <TodoForm onAddTodo={this.handleAddTodo} />
        {todos}
       </div>
       </div>

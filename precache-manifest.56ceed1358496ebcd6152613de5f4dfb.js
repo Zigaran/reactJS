75e86c70d03b21c775a9b698b92e9779358e32fd
natan <eliasnatanzigaran@hotmail.com>
2020-03-07 10:36:29 -0300

    Updates

---
+++ precache-manifest.56ceed1358496ebcd6152613de5f4dfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a428d959eb708815251485c1a2a9f0d",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "c11c5f4fe7ed55e2949a",
+    "url": "/ReactJS/static/css/main.dca64d0f.chunk.css"
+  },
+  {
+    "revision": "c7f4b84ab66f15a23e3c",
+    "url": "/ReactJS/static/js/2.0399eb33.chunk.js"
+  },
+  {
+    "revision": "c11c5f4fe7ed55e2949a",
+    "url": "/ReactJS/static/js/main.8bede4f4.chunk.js"
+  },
+  {
+    "revision": "ccd1ca3e8aa5e70a4e03",
+    "url": "/ReactJS/static/js/runtime~main.f8a3c22c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
